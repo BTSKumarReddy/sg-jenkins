eea96aed8c277d38fb02e835bd5dd20d8349ede7
BTSKumarReddy <bukkasamudramtharun@gmail.com>
2021-12-06 14:15:00 +0530

    Second commit

--- src/main/java/com/example/mydemoapp/controller/DemoController.java
+++ src/main/java/com/example/mydemoapp/controller/DemoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,9 @@ public class DemoController {
     {
         return ResponseEntity.ok("Hello from hellocontroller Listesing on Port: ");
     }
-
+    @GetMapping("/hello")
+     public ResponseEntity<?> sayHello(@RequestParam("message") String message)
+    {
+        return ResponseEntity.ok("hello "+message);
+    }
 }
