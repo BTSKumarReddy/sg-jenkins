dfd3529c82ba56395c408299bf1de79ad57ebccd
BTSKumarReddy <bukkasamudramtharun@gmail.com>
2021-12-06 12:53:33 +0530

    first commit

---
+++ src/main/java/com/example/mydemoapp/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.mydemoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Autowired
+    private  Environment environment;
+
+    @GetMapping("/")
+    public ResponseEntity<?> getStatus()
+    {
+        return ResponseEntity.ok("Hello from hellocontroller Listesing on Port: ");
+    }
+
+}
